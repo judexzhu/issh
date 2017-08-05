c585cdb9e52650d95b9c3bd4f75ed3c2183af47a
lon <dove2580@live.com>
2017-08-05 21:47:22 +0800

    Add UserCtrl

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,25 @@
-angular.module("iSSH", [])
-  .controller("MainCtrl", function ($scope) {
+var app = angular.module('iSSH', []);
 
-    $scope.toggleSidebar = function () {
-      $('body').toggleClass('mini-sidebar');
-    }
+app.controller('MainCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
 
+  $scope.toggleSidebar = function () {
+    $('body').toggleClass('mini-sidebar');
+  }
+
+  $scope.$on('UserCtrlChange', function (event, msg) {
+    $rootScope.isLogin = !!msg.isLogin;
   });
+
+}]);
+
+app.run(['$rootScope', '$http', function ($rootScope, $http) {
+  $http.get('/api/user')
+    .then(function (res) {
+      if (res.data) {
+        $rootScope.isLogin = true;
+        $rootScope.UserInfo = res.data;
+      }
+    }, function (errRes) {
+      $rootScope.isLogin = false;
+    });
+}]);
---
+++ src/public/js/user.js
@@ -0,0 +1,59 @@
+app.controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
+
+  $scope.submitted = false;
+
+  $scope.init = function () {
+    $scope.signinInfo = {
+      username: '',
+      password: ''
+    }
+
+    $scope.registerInfo = {
+      userName: '',
+      password: '',
+      displayName: '',
+      email: ''
+    }
+
+    $scope.submitted = false;
+  }
+
+  $scope.init();
+
+  $scope.showSignIn = function () {
+    $('.signin-panel').slideDown();
+    $('.register-panel').slideUp();
+    $scope.init();
+  }
+
+  $scope.signin = function () {
+    $scope.submitted = true;
+    if ($scope.signinForm.$invalid) return;
+    $http.post('/api/user/login', $scope.signinInfo)
+      .then(function (res) {
+        toastr.success("Sign in suceed.");
+        $scope.$emit("UserCtrlChange", { isLogin: true });
+      }, function (errRes) {
+        toastr.error('Username or Password is not correct');
+      });
+  }
+
+  $scope.showRegister = function () {
+    $('.signin-panel').slideUp();
+    $('.register-panel').slideDown();
+    $scope.init();
+  }
+
+  $scope.register = function () {
+    $scope.submitted = true;
+    if ($scope.registerForm.$invalid) return;
+    $http.post('/api/user/register', $scope.registerInfo)
+      .then(function (res) {
+        toastr.success('Register succeed');
+        $scope.showSignIn();
+      }, function (errRes) {
+        toastr.error('Register failed');
+      });
+  }
+
+}]);
