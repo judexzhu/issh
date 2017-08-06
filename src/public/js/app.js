3326bcb504b868be00316ddbf1f06c60c55a921d
winifredTomlinson <sw6l@buyabs.corp>
2017-08-06 11:53:58 +0800

    接入sso

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('iSSH', []);
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+}]);
 
-app.run(['$rootScope', '$http', function ($rootScope, $http) {
+app.run(['$rootScope', '$http','$location', function ($rootScope, $http, $location) {
   $rootScope.isLogin = false;
   $.ajax({
     url: '/api/user',
@@ -12,7 +18,25 @@ app.run(['$rootScope', '$http', function ($rootScope, $http) {
       }
     },
     error: function (errData) {
-
+      console.log('===============');
+      $rootScope.token = $location.search().t;
+      console.log($rootScope.token);
+      if($rootScope.token){
+        $.ajax({
+          url: '/api/user/ssologin',
+          method: 'post',
+          data: JSON.stringify({token: $rootScope.token}),
+          contentType: 'application/json',
+          async: false,
+          success: function (data) {
+            if (data) {
+              $rootScope.isLogin = true;
+              $rootScope.UserInfo = data;
+              $location.url($location.path());
+            }
+          },
+        })
+      }
     }
   });
 }]);
